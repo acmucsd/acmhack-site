a665c9c9884c2a4934dde651d47736c36f879939
annsudhart <asudhart@ucsd.edu>
2020-09-16 16:44:23 -0700

    fixed previous workshop bit

--- src/pages/Workshops/index.tsx
+++ src/pages/Workshops/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from '../../components/NavBar';
 import HackEventList from 'containers/HackEvents/EventsListContainer';
+import Description from 'components/Description';
 import './style.less';
 
 const Workshops: React.FC = () => {
@@ -8,19 +9,10 @@ const Workshops: React.FC = () => {
     <div className="page workshops">
       <Navbar />
       <div className="container workshop-content">
-        <h1>
-          Previous Workshops
-        </h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-          Vitae augue curabitur in sapien, volutpat lacus. Eros dui 
-          ridiculus augue ultrices blandit feugiat egestas in lectus. 
-          Scelerisque velit proin eu ullamcorper feugiat massa tempus, 
-          amet vulputate. Morbi adipiscing condimentum et a, enim 
-          facilisis ultricies mention workshop series somewhere 
-          with a link like Hack.Py, HackSchool, Etc. Ideally these 
-          would be a link to the events for that series.
-        </p>
+        <Description
+          title="Previous Workshops"
+          content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse blandit nibh sed elit imperdiet, eget pharetra mi finibus. Sed consectetur ac erat quis eleifend. Aenean placerat suscipit nisl ac congue. Cras commodo in ante eu porttitor. Donec a finibus elit. Ut consequat, risus nec iaculis consequat, nulla tellus ultricies lectus, a facilisis nibh massa vel turpis. Mauris dictum dignissim ornare. Aliquam blandit purus erat. Nulla facilisi. Pellentesque sagittis lorem vel feugiat volutpat. Integer ac lacinia ante."
+        />
         <h2>
           Hack.Py
         </h2>
