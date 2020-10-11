23e02bf1d1dfaa1beed81113df51ab8b8539281b
annsudhart <asudhart@ucsd.edu>
2020-10-11 12:57:10 -0700

    removed unneccesary useeffect

--- src/containers/HackEvents/EventsListContainer/index.tsx
+++ src/containers/HackEvents/EventsListContainer/index.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { getHackData, EventObject } from 'actions/events';
+import React from 'react';
+import { getHackData } from 'actions/events';
 import HackEventList from 'components/HackEvents/HackList';
 
 type EventsListContainerProps = { 
@@ -10,10 +10,11 @@ type EventsListContainerProps = {
   type: string
 }
 const EventsListContainer = ({ limit, type }: EventsListContainerProps) => {
-  const [events, setEvents] = useState<Array<EventObject>>([]);
+  /* const [events, setEvents] = useState<Array<EventObject>>([]);
   useEffect(() => {
     setEvents(getHackData(type).slice(0, limit));
-  }, [limit]);
+  }, [limit]); */
+  let events = getHackData(type).slice(0, limit);
   return (
     <HackEventList type={type} events={events} />
   );
