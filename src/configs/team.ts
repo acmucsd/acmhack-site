c77079dea138b258b412766a7fb0f150f88e7f4d
Garrett Luu <garrettluu131@gmail.com>
2021-01-12 21:21:55 -0800

    Add board info (#17)

--- src/configs/team.ts
+++ src/configs/team.ts
@@ -1,39 +1,17 @@
-import emily from "../assets/people/emily.png"
-import jennifer from "../assets/people/jennifer.png"
-import ronak from "../assets/people/ronak.jpg"
-import daniel from "../assets/people/daniel.jpg"
+import daniel from "../assets/people/daniel.jpg";
+import garrett from "../assets/people/garrett.jpg";
+import david from "../assets/people/david.jpg";
+import shravan from "../assets/people/shravan.jpg";
+import henry from "../assets/people/henry.jpg";
+import maggie from "../assets/people/maggie.jpg";
+import thomas from "../assets/people/thomas.png";
+import trisa from "../assets/people/trisa.jpg";
+import emily from "../assets/people/emily.jpeg";
 
 export const team = [
 	{
-		name: "Emily",
-		role: "Supreme Leader",
-		profilePicture: emily,
-		plugs: {
-			linkedin: "emilyhuongnguyen",
-			github: "eminguyen"
-		}
-	},
-	{
-		name: "Jennifer",
-		role: "Developer",
-		profilePicture: jennifer,
-		plugs: {
-			linkedin: "jennifer-pham-575ab113b/",
-			github: "Froggerson"
-		}
-	},
-	{
-		name: "Ronak",
-		role: "PM",
-		profilePicture: ronak,
-		plugs: {
-			linkedin: "trulyronak",
-			github: "trulyronak"
-		}
-	},
-	{
 		name: "Daniel",
-		role: "QA",
+		role: "Co-President",
 		profilePicture: daniel,
 		plugs: {
 			instagram: "daniel14truong",
@@ -41,5 +19,76 @@ export const team = [
 			github: "daniel-d-truong"
 		}
 	},
-	
+	{
+		name: "Garrett",
+		role: "Co-President",
+		profilePicture: garrett,
+		plugs: {
+			linkedin: "garrettluu",
+			github: "garrettluu"
+		}
+	},
+	{
+		name: "David",
+		role: "Technical Event Lead, Hack.py Director",
+		profilePicture: david,
+		plugs: {
+			linkedin: "davidgdempsey",
+			github: "daviddempsey"
+		}
+	},
+	{
+		name: "Shravan",
+		role: "Technical Event Lead, Hack School Director",
+		profilePicture: shravan,
+		plugs: {
+			linkedin: "shravanhariharan",
+			github: "shravanhariharan",
+		}
+	},
+	{
+		name: "Henry",
+		role: "Technical Event Lead",
+		profilePicture: henry,
+		plugs: {
+			linkedin: "henry-c",
+			github: "chanhenry54",
+		}
+	},
+	{
+		name: "Maggie",
+		role: "Technical Event Lead",
+		profilePicture: maggie,
+		plugs: {
+			linkedin: "yiyuemaggiemao",
+			github: "YiyueMaggieMao"
+		}
+	},
+	{
+		name: "Thomas",
+		role: "Technical Event Lead",
+		profilePicture: thomas,
+		plugs: {
+			linkedin: "thomas-garry-55c",
+			github: "thomas-gg"
+		}
+	},
+	{
+		name: "Trisa",
+		role: "Technical Event Lead",
+		profilePicture: trisa,
+		plugs: {
+			linkedin: "trisa-leung-58384a183",
+			github: "trisaleung"
+		}
+	},
+	{
+		name: "Emily",
+		role: "QA Director",
+		profilePicture: emily,
+		plugs: {
+			linkedin: "em-chin",
+			github: "emchin"
+		}
+	},
 ]
